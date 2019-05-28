e17d5f67823c1ae232a9ff28ff72c22b52a53409
Justina Vickers <justinav38@gmail.com>
2019-05-28 13:07:37 -0500

    edit in the details option auto fills with the correct information, I needed to uncomment code

---
+++ StudentExercisesMVC/Controllers/CohortsController.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentExercisesMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesMVC.Models;
+
+namespace StudentExercisesMVC.Controllers
+
+{
+    public class CohortsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public CohortsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Cohort
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id,
+                                c.Designation
+                            FROM Cohort c
+                        ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Cohort> cohorts = new List<Cohort>();
+                    while (reader.Read())
+                    {
+                        Cohort cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation"))
+                           
+                        };
+
+                        cohorts.Add(cohort);
+                    }
+
+                    reader.Close();
+
+                    return View(cohorts);
+                }
+            }
+        }
+
+        // GET: Cohort/Details/5
+        public ActionResult Details(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id,
+                                c.Designation                   
+                            FROM Cohort c
+                            WHERE c.Id = @CohortId
+                        ";
+                    cmd.Parameters.Add(new SqlParameter("@CohortId", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Cohort cohort = null;
+                    if (reader.Read())
+                    {
+                        cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation"))
+                       
+                        };
+                    }
+
+                    reader.Close();
+
+                    return View(cohort);
+                }
+            }
+        }
+
+        // GET: Cohort/Create
+        public ActionResult Create()
+        {
+            InstructorCreateViewModel model = new InstructorCreateViewModel(Connection);
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id,
+                                c.Designation
+                            FROM Cohort c
+                        ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Cohort cohort = null;
+                    if (reader.Read())
+                    {
+                        cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation"))
+                        };
+                    }
+
+                    reader.Close();
+
+                    return View(model);
+                }
+            }
+        }
+
+        // POST: Cohort/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Cohort cohort)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Cohort (Designation)         
+                                         OUTPUT INSERTED.Id                                                       
+                                         VALUES (@designation)";
+                    cmd.Parameters.Add(new SqlParameter("@designation", cohort.Designation));
+ 
+
+                    int newId = (int)cmd.ExecuteScalar();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+        }
+
+        // GET: Cohort/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Cohort cohort = null;
+            using (SqlConnection conn = Connection)
+
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id,
+                                c.Designation
+                            FROM Cohort c
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation")),
+                          
+
+                        };
+                    }
+
+                    reader.Close();
+
+                }
+            }
+            return View(cohort);
+        }
+
+        // POST: Cohort/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([FromForm] Cohort cohort)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Cohort
+                                           SET Designation = @designation
+                                           WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@designation", cohort.Designation));
+
+
+                        cmd.ExecuteNonQuery();
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        // GET: Cohort/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id,
+                                c.Designation
+                            FROM Cohort c
+                            WHERE c.Id = @CohortId
+                        ";
+                    cmd.Parameters.Add(new SqlParameter("@CohortId", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Cohort cohort = null;
+                    if (reader.Read())
+                    {
+                        cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Designation = reader.GetString(reader.GetOrdinal("Designation")),
+                          
+                        };
+                    }
+
+                    reader.Close();
+
+                    return View(cohort);
+                }
+            }
+        }
+
+        // POST: Cohort/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Cohort WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
